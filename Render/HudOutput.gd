a324ad7fc1b1880b141e2ef8859f8b81230841f8
Liam <Mooshrew.haze@gmail.com>
2023-10-24 17:43:31 -0400

    Improved parsing
    Can now select specific things to render

--- Render/HudOutput.gd
+++ Render/HudOutput.gd
@@ -4,6 +4,8 @@ extends Control
 @export var ResW : int = 640
 
 @onready var element_dict = $Res_Reader.Res_dict
+@onready var option = $Editor/ItemList
+var selected = 0
 
 func _ready():
 	pass
@@ -18,7 +20,6 @@ func add_element(element):
 	var t = 0
 	var test_element = ColorRect.new()
 	for i in element_dict[element]:
-			print(i)
 			if i[0] == "\t\txpos":
 				test_element.position.x = int(i[1])
 			if i[0] == "\t\typos":
@@ -32,8 +33,17 @@ func add_element(element):
 				t = int(i[1])
 			test_element.reparent($HUD,false)
 			test_element.set_size(Vector2(w,t))
-			test_element.color = Color(1, 1, 1, 1)
+			test_element.color = Color(randf_range(0,1), randf_range(0,1), randf_range(0,1), 0.05)
 			add_child(test_element)
 			
 func _on_button_pressed():
-	inter_dict()
+	print(selected)
+	add_element(element_dict.keys()[selected])
+
+
+func _on_item_list_item_selected(index):
+	selected = index
+
+func _on_button_2_pressed():
+	for i in element_dict.keys():
+		option.add_item(str(i))
--- test.gd
+++ test.gd
@@ -2,23 +2,29 @@ extends Node
 
 
 func _ready():
-	print(get_var_array("		\"fieldName\" \"HudPlayerStatus\""))
+	print(get_var_array("		\"tall\"	\"60\""))
 	
 func get_var_array(line):
-	var count : int = 0
+	var Routput  = ""
 	var output = ""
-	var Routput = ""
-	var line_l = line.length() - 1
-	while count < 2:
-		if line[line_l] == "\"":
-			count += 1
+	var t
+	for i in range(line.length() - 1, -1, -1):
+		Routput += line[i]
+	t = get_name_array(Routput)
+	for u in range(t.length() - 1, -1, -1):
+		output += t[u]
+	return output
+	
+func get_name_array(line):
+	var c : int = 0
+	var p : int = 0
+	var output = ""
+	while p < 2 and c < line.length():
+		if line[c] == "\"" :
+			p += 1
 		else:
-			Routput += line[line_l]
-		line_l -= 1	
-	var Routput_l = Routput.length() - 1
-	while Routput_l > -1:
-		output += Routput[Routput_l]
-		Routput_l += -1
+			output += line[c]
+		c += 1
 	return output
 
 func VEREW(line):
